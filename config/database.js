854e928712086cd66b42edd23b3d5bc104ec1b63
Dominic Zenon <dominiczenon@gmail.com>
2017-08-23 14:59:36 -0700

    up to authentications

--- config/database.js
+++ config/database.js
@@ -1,8 +1,9 @@
-const crypto = require('crypto').randomBytes(256).toString('hex');
+const crypto = require('crypto').randomBytes(256).toString('hex'); // Provides
+// cryptographic functionality (OpenSSL's hash, HMAC, cipher, decipher, sign and verify functions)
 
-
+// Export config object
 module.exports = {
-    uri: 'mongodb://localhost:27017/'+ this.db,
-    secret: crypto,
-    db: 'mean-angular-2'
+    uri: 'mongodb://localhost:27017/clearfreight_db', //Database URI and database name
+    secret: crypto, // Crypto-create secret
+    db: 'clearfreight_db' //Database name
 }
